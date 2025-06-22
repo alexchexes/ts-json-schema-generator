95fa0ec27114152fbb9698712b04deb4f6b17fb9
alexchexes <alexchexes@gmail.com>
2025-06-22 22:42:33 +0100

    add test fixture

---
+++ test/valid-data/temp-test-folder-1/main.ts
@@ -0,0 +1,43 @@
+export type TopLevelSpec = {
+    view?: BaseViewBackground<SignalRef>;
+    config?: Config;
+};
+
+export interface BaseViewBackground<S extends SignalRef> extends Partial<Pick<MarkConfig<S>, "strokeJoin">> {}
+
+export interface Config extends MarkConfigMixins<ExprRef | SignalRef>, VgMarkConfig {}
+
+export interface MarkConfig<ES extends ExprRef | SignalRef>
+    extends MapExcludeValueRefAndReplaceSignalWith<VgMarkConfig, ES> {}
+
+export interface MarkConfigMixins<ES extends ExprRef | SignalRef> {
+    area?: MarkConfig<ES>;
+}
+
+export type MapExcludeAndKeepSignalAs<T, E, ES extends ExprRef | SignalRef> = {
+    [P in keyof T]: SignalRef extends T[P] ? Exclude<T[P], E> | ES : Exclude<T[P], E>;
+};
+
+export type MapExcludeValueRefAndReplaceSignalWith<T, ES extends ExprRef | SignalRef> = MapExcludeAndKeepSignalAs<
+    T,
+    BaseValueRef<any>,
+    ES
+>;
+
+export interface ExprRef {
+    expr?: string;
+}
+
+export type BaseValueRef<T> = SignalRef | { value: T | null };
+
+export interface VgMarkConfig {
+    /** IF WE REMOVE THIS JSDOC WITHOUT REMOVING `SignalRef`, GENERATOR CRASHES (!). If replace to string and del JSDod - def name changes */
+    strokeJoin?: SignalRef;
+}
+
+/**
+ * @hidden
+ */
+export interface SignalRef {
+    signal?: string;
+}
