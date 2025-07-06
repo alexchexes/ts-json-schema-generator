00fb34bf53666199eee6f7692d3337fcf538de05
alexchexes <alexchexes@gmail.com>
2025-07-06 11:01:50 +0100

    add test case 'mapped-intersection-complex'

--- test/config.test.ts
+++ test/config.test.ts
@@ -399,6 +399,14 @@ describe("config", () => {
     );
 
     it(
+        "mapped-intersection-complex",
+        assertSchema("mapped-intersection-complex", {
+            type: "MyObject",
+            additionalProperties: true,
+        }),
+    );
+
+    it(
         "arrow-function-parameters",
         assertSchema("arrow-function-parameters", {
             type: "myFunction",
