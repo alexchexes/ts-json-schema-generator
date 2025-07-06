fe69148caff0a8414e8d42a6e872100e66b848bd
Alex <alexchexes@gmail.com>
2025-07-06 19:04:01 +0100

    feat: inline single-use generic structures

--- src/SchemaGenerator.ts
+++ src/SchemaGenerator.ts
@@ -11,6 +11,7 @@ import type { StringMap } from "./Utils/StringMap.js";
 import { hasJsDocTag } from "./Utils/hasJsDocTag.js";
 import { removeUnreachable } from "./Utils/removeUnreachable.js";
 import { symbolAtNode } from "./Utils/symbolAtNode.js";
+import { inlineSingleUse } from "./Utils/inlineSingleUse.js";
 
 export class SchemaGenerator {
     public constructor(
@@ -52,11 +53,13 @@ export class SchemaGenerator {
             {},
         );
 
+        const finalDefinitions = inlineSingleUse(rootTypeDefinition, reachableDefinitions);
+
         return {
             ...(this.config?.schemaId ? { $id: this.config.schemaId } : {}),
             $schema: "http://json-schema.org/draft-07/schema#",
             ...(rootTypeDefinition ?? {}),
-            definitions: reachableDefinitions,
+            definitions: finalDefinitions,
         };
     }
 
